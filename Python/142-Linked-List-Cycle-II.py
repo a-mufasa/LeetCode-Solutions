1f5ff3cf7b3222e4346eb94861719e343521c86a
a-mufasa <ahmedr.moustafa@gmail.com>
2023-01-07 14:50:46 -0600

    1-7-23 update

---
+++ Python/142-Linked-List-Cycle-II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        if not fast or not fast.next:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return fast
