fab8ef24cecdb56024774a4b403cd385809217d6
a-mufasa <ahmedr.moustafa@gmail.com>
2023-01-26 22:15:31 -0600

    1-26-2023 update

---
+++ Python/98-Validate-Binary-Search-Tree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def inOrderCheck(node, min_val, max_val):
+            if node is None:
+                return True
+            if node.val <= min_val or node.val >= max_val:
+                return False
+            return inOrderCheck(node.left, min_val, node.val) and inOrderCheck(node.right, node.val, max_val)
+
+        return inOrderCheck(root, float('-inf'), float('inf'))
